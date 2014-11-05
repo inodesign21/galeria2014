86f4d3d44d31f603a3596f1ee8221178df4569b9
inodesign21 <inodesign21@gmail.com>
2014-11-05 20:04:22 -0200

    update galeria

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
- window.onload = function () {
+// Iniciando galeria
+blueimp.Gallery(
+    document.getElementById('links'), {
+        onslide: function (index, slide) {
+            var text = this.list[index].getAttribute('data-description'),
+                node = this.container.find('.description');
+            node.empty();
+            if (text) {
+                node[0].appendChild(document.createTextNode(text));
+            }
+        }
+});
 
-// Gallery
 document.getElementById('links').onclick = function (event) {
-event = event || window.event;
-var target = event.target || event.srcElement,
-    link = target.src ? target.parentNode : target,
-    options = {index: link, event: event},
-    links = this.getElementsByTagName('a');
-blueimp.Gallery(links, options);
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event, onslide: function (index, slide) {
+            var text = this.list[index].getAttribute('data-description'),
+                node = this.container.find('.description');
+            node.empty();
+            if (text) {
+                node[0].appendChild(document.createTextNode(text));
+            }
+        } },
+        links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options);
 };
 
-};  
-        
-        
-// My app 
+// My app
+
+//Img responsive class of bootstrap
+$('#links img').addClass('img-responsive');
